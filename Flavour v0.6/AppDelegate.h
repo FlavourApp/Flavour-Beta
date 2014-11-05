49338d085613838f4d9fe74838075bef4f2142d4
Demian Schkolnik <demianschkolnik@gmail.com>
2014-11-05 20:24:42 -0300

    Initial Commit

---
+++ Flavour v0.6/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flavour v0.6
+//
+//  Created by Demian Schkolnik on 11/5/14.
+//  Copyright (c) 2014 Schkolnik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
